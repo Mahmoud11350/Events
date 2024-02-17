dc08ae8c0e34ce8cd53a25652c48097cb3883e5a
mahmoud11350 <mahmoudsalah11350@gmail.com>
2024-02-17 18:13:57 +0200

    token bug

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,14 +3,18 @@ import ERRORHANDLER from "../errors/errors.js";
 import { verifyToken } from "../utils/tokenLibs.js";
 
 export const authMiddleware = async (req, res, next) => {
-  const token = req.signedCookies.token;
+  // const token = req.signedCookies.token;
   const Token = req.cookie.token;
-  if (!token) {
-    throw new ERRORHANDLER(
-      `there are no token Token : ${Token} & token : ${token}`,
-      StatusCodes.BAD_REQUEST
-    );
-  }
+  throw new ERRORHANDLER(
+    `there are no token Token : ${Token}`,
+    StatusCodes.BAD_REQUEST
+  );
+  // if (!token) {
+  //   throw new ERRORHANDLER(
+  //     `there are no token Token : ${Token} & token : ${token}`,
+  //     StatusCodes.BAD_REQUEST
+  //   );
+  // }
   try {
     const user = await verifyToken({ token });
     req.user = user;
